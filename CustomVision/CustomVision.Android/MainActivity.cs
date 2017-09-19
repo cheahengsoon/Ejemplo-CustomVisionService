9ccbd8a2f286fb7f12d9fb1e1153666bc4e7c42f
Jesus Angulo <jesus.angulo@outlook.com>
2017-09-18 20:13:02 -0500

    Finished

--- CustomVision/CustomVision.Android/MainActivity.cs
+++ CustomVision/CustomVision.Android/MainActivity.cs
@@ -1,11 +1,8 @@
-﻿using System;
-
+﻿
 using Android.App;
 using Android.Content.PM;
-using Android.Runtime;
-using Android.Views;
-using Android.Widget;
 using Android.OS;
+using Plugin.Permissions;
 
 namespace CustomVision.Droid
 {
@@ -19,9 +16,16 @@ namespace CustomVision.Droid
 
             base.OnCreate(bundle);
 
+            Acr.UserDialogs.UserDialogs.Init(this);
             global::Xamarin.Forms.Forms.Init(this, bundle);
             LoadApplication(new App());
         }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, Permission[] grantResults)
+        {
+            PermissionsImplementation.Current.OnRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+
     }
 }
 
--- CustomVision/CustomVision/MainPage.xaml.cs
+++ CustomVision/CustomVision/MainPage.xaml.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using Plugin.Media.Abstractions;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
@@ -9,24 +12,83 @@ namespace CustomVision
 {
     public partial class MainPage : ContentPage
     {
+        private MediaFile _foto = null;
         public MainPage()
         {
             InitializeComponent();
         }
 
-        private void ElegirImage(object sender, EventArgs e)
+        private async void ElegirImage(object sender, EventArgs e)
         {
+            await Plugin.Media.CrossMedia.Current.Initialize();
 
+            _foto = await Plugin.Media.CrossMedia.Current.PickPhotoAsync(new Plugin.Media.Abstractions.PickMediaOptions());
+            Img.Source = FileImageSource.FromFile(_foto.Path);
         }
 
-        private void TomarFoto(object sender, EventArgs e)
+        private async void TomarFoto(object sender, EventArgs e)
         {
+            await Plugin.Media.CrossMedia.Current.Initialize();
 
+            _foto = await Plugin.Media.CrossMedia.Current.TakePhotoAsync(new StoreCameraMediaOptions()
+            {
+                Directory = "Vision",
+                Name = "Target.jpg"
+            });
+            Img.Source = FileImageSource.FromFile(_foto.Path);
         }
 
-        private void Clasificar(object sender, EventArgs e)
+        private async void Clasificar(object sender, EventArgs e)
         {
+            using (Acr.UserDialogs.UserDialogs.Instance.Loading("Clasificando..."))
+            {
+                if (_foto == null) return;
 
+                var stream = _foto.GetStream();
+
+                var httpClient = new HttpClient();
+                var url = "https://southcentralus.api.cognitive.microsoft.com/customvision/v1.0/Prediction/701b4eed-c0b1-4f4d-86af-873a7b4ad6a3/image";
+                httpClient.DefaultRequestHeaders.Add("Prediction-Key", "c1561b9c08134ce69773e368ace40fe6");
+
+                var content = new StreamContent(stream);
+
+                var response = await httpClient.PostAsync(url, content);
+
+                if (!response.IsSuccessStatusCode)
+                {
+
+                }
+
+                var json = await response.Content.ReadAsStringAsync();
+
+                var c = JsonConvert.DeserializeObject<ClasificationResponse>(json);
+
+                var p = c.Predictions.FirstOrDefault();
+                if (p == null) Acr.UserDialogs.UserDialogs.Instance.Toast("Image not found");
+
+                ResponseLabel.Text = $"{p.Tag} - {p.Probability:p0}";
+                Accuracy.Progress = p.Probability;
+            }
+
+            Acr.UserDialogs.UserDialogs.Instance.Toast("Clasification finished...");
         }
     }
+
+
+    public class ClasificationResponse
+    {
+        public string Id { get; set; }
+        public string Project { get; set; }
+        public string Iteration { get; set; }
+        public DateTime Created { get; set; }
+        public Prediction[] Predictions { get; set; }
+    }
+
+    public class Prediction
+    {
+        public string TagId { get; set; }
+        public string Tag { get; set; }
+        public float Probability { get; set; }
+    }
+
 }
